d8053ee4de0945c33cdefe412b305879cc46eae7
Endlesstankzone <tankhouse14@hotmail.com>
2017-11-26 12:27:04 -0700

    Updated Distance code

--- Distance/Distance.ino
+++ Distance/Distance.ino
@@ -2,31 +2,42 @@
 
 #define trigPin 13
 #define echoPin 12
-#define led2 10
+#define Rled 10
+#define Gled 11
 
 void setup() {
   Serial.begin (9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(Rled, OUTPUT);
+  pinMode(Gled, OUTPUT);
 }
 
 void loop() {
   long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, LOW);  
+  delayMicroseconds(2); 
   digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(1000); - Removed this line
-  delayMicroseconds(10); // Added this line
+  delayMicroseconds(10); 
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
 
-
-
-
-
-
-
+  if (distance > 20 && distance < 24) {
+    digitalWrite(Gled, HIGH);
+    digitalWrite(Rled, LOW);
+  } else {
+    digitalWrite(Gled, LOW);
+    digitalWrite(Rled, HIGH);
+  }
  
+  if (distance > 30) {
+    Serial.println("Out Of Range");
+  }
+
+
+  delay(50);
+
+
+
 }
